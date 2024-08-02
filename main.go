78ff4bb71aa847f7156322ad6c7738fc415b3103
Ken <kbcontactme@gmail.com>
2024-08-02 19:15:03 -0400

    WalkCommands

--- main.go
+++ main.go
@@ -20,12 +20,15 @@ func main() {
 func Run() {
 	var running int = 1
 	var character string = ""
-	url := ""
+	actionPost := menus.ActionResponse{
+		Url:     "",
+		PostGet: "",
+		Data:    "",
+	}
 	reader := bufio.NewReader(os.Stdin)
 
 	for running == 1 {
-		// Print the instruction to the reader in the console
-
+		// Print Character list
 		fmt.Println(menus.GetCharacterMenu())
 
 		// Call the reader to read user's input
@@ -36,34 +39,45 @@ func Run() {
 		key = strings.TrimSpace(key)
 		character = menus.GetCharacter(key)
 		fmt.Println(menus.GetActionsMenu())
+
 		// Call the reader to read user's input
 		key2, err := reader.ReadString('\n')
 		if err != nil {
 			panic(err)
 		}
 		key2 = strings.TrimSpace(key2)
-		url = menus.GetAction(key2, character)
-		// yes yes this has been changed
-		var bearer = "Bearer " + "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6ImRhZGVidHNpbXVsYXRvciIsInBhc3N3b3JkX2NoYW5nZWQiOiIyMDI0LTA4LTAyIDE1OjAzOjIxLjM3MzUzNCJ9.gcnIvBLjqesqXVBQ6zSr78ELK6B-trsjDWRtCr-DgPg"
-		req, err := http.NewRequest("POST", url, nil)
+		actionPost = menus.GetAction(key2, character)
+
+		var bearer = "Bearer " + "PlaceHolder"
 		if err != nil {
-			log.Println("Error trying to make post request")
+			fmt.Println("Can't marshal jsonvalue: ", actionPost.Data)
 		}
-		req.Header.Add("Accept", "application/json")
-		req.Header.Add("Content-Type", "application/json")
-		req.Header.Add("Authorization", bearer)
 
 		client := &http.Client{}
+		req := HttpReqBuilder(actionPost, bearer)
 		resp, err := client.Do(req)
 		if err != nil {
 			log.Println("Error on response.\nERROR] -", err)
 		}
 		defer resp.Body.Close()
-
 		body, err := io.ReadAll(resp.Body)
 		if err != nil {
 			log.Println("Error while reading the response bytes:", err)
 		}
-		parse.ParseCombatLog(body)
+		parse.ParseOperator(key2, body)
 	}
+}
+
+func HttpReqBuilder(action menus.ActionResponse, bearer string) *http.Request {
+	payload := strings.NewReader("{\n  \"x\": 0,\n  \"y\": 0\n}")
+	req, err := http.NewRequest(action.PostGet, action.Url, payload)
+	fmt.Println(action)
+	if err != nil {
+		fmt.Println("Fail build request with json")
+		fmt.Println(err)
+	}
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Authorization", bearer)
+	return req
 }
--- menuMaps/menuMaps.go
+++ menuMaps/menuMaps.go
@@ -1,11 +1,25 @@
 package menuMaps
 
+import (
+	"fmt"
+)
+
+type ActionResponse struct {
+	Url     string
+	PostGet string
+	Data    string
+}
+
+func NewEmptyActionResponse() ActionResponse {
+	return ActionResponse{Url: "", PostGet: "", Data: ""}
+}
+
 func GetCharacter(val string) string {
 	// return Url for relevant api
 	switch val {
 	// Character
 	case "0":
-		return "Guile"
+		return "DaGuile"
 	case "1":
 		return "PlaceHolder"
 	default:
@@ -30,17 +44,30 @@ func GetActionsMenu() string {
 }
 
 // Call the function to build your url
-func GetAction(val string, char string) string {
+func GetAction(val string, char string) ActionResponse {
 	switch val {
 	case "0":
-		return ""
+		return Walk(char)
 	case "1":
 		return Attack(char)
 	default:
-		return ""
+		return NewEmptyActionResponse()
 	}
 }
 
-func Attack(char string) string {
-	return "https://api.artifactsmmo.com/my/DaGuile/action/fight"
+func Attack(char string) ActionResponse {
+	return ActionResponse{Url: "https://api.artifactsmmo.com/my/" + char + "/action/fight", PostGet: "POST", Data: ""}
+}
+
+func Walk(char string) ActionResponse {
+	var x string
+	var y string
+
+	fmt.Println("Enter x y location")
+	_, err := fmt.Scan(&x, &y)
+	if err != nil {
+		fmt.Println("X,Y Error:", err)
+	}
+	data := `\n  \"x\":` + x + `\n  \"y\":` + y + `\n`
+	return ActionResponse{Url: "https://api.artifactsmmo.com/my/" + char + "/action/move", PostGet: "POST", Data: data}
 }
